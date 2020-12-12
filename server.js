8876ceadc152e8f8f58571ce820c1bcfa4535133
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 11:52:12 -0500

    Add boilerplate code server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+//Boilerplate language for express, handlebars, and mysql
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+// Starting up the server
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
