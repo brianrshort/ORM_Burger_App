3a7f98716147d434e3ac79440348bad6cb2a254b
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 13:19:57 -0500

    Add JAWSDB info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //Boilerplate to require mysql and connect to the database
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "burger_city_db"
-});
+var connection 
+
+if (process.env.JASDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burger_city_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
