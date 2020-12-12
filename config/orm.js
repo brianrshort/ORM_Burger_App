6decc49769d7816fea73a60ccc2ff47e7586b8bb
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 11:56:26 -0500

    Add helper function to orm.js to translate js to sql query language

--- config/orm.js
+++ config/orm.js
@@ -12,3 +12,18 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
+// Another helper function to translate js objects into SQL query language
+function objToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
