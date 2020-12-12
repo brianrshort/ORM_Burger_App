519221fbba9744526f94907d3dab703f3aa8dbe7
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 12:01:42 -0500

    Add read all function for main page in controller file

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,3 +4,14 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+//Read all entries in the burgers table on the main page
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
