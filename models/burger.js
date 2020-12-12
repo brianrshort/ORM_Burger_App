6f179a32b6256536309e8115e90e2ddba0dfe69c
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 12:06:41 -0500

    Add read all method using ORM in burger.js burger object

--- models/burger.js
+++ models/burger.js
@@ -2,3 +2,12 @@ var orm = require("../config/orm.js");
 
 //A model object that uses the ORM to manipulate, add, and destroy information in the burgers table
 var burger = {
+
+  //Read all burgers table
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  
