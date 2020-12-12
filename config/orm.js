260675c1569d31d477ea24b9af801cda664c43fb
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 11:55:50 -0500

    Add helper function to orm.js to manipulate questions marks in queries

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,14 @@
 //Requiring the connection to get to the database
 var connection = require("../config/connection.js");
 
+//Helper function to pull in a specific number of questions marks to manipulate and shape the SQL queries
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
