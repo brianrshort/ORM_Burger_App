bc7183ef197f2547bef2e95a47920bedff752474
Brian Short <brian.ralph.short@gmail.com>
2020-12-12 11:58:19 -0500

    Add update function on orm object

--- config/orm.js
+++ config/orm.js
@@ -63,4 +63,23 @@ var orm = {
     });
   },
 
+  //Update an existing row in a table in the database
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+
   
